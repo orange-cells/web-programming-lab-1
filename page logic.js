1ad54a97b106b35ba8b45e5a13eb5f1b0f7192ca
orange-cells <sokolovaaveraa@gmail.com>
2025-11-27 00:44:37 +0300

    реализовала открытие/закрытие меню
    честно говоря, я не помню, было оно реализовано или нет, но у меня точно что-то лагало, пока я не перезагрузила

--- page logic.js
+++ page logic.js
@@ -1,6 +1,10 @@
 ﻿let shoppingCartImg = document.querySelector('.cart-image');
+let closeButton = document.querySelector('.shoppingCart .close')
 let body = document.querySelector('body');
 
 shoppingCartImg.addEventListener('click', () => {
-    body.classList.toggle('showCart')
+    body.classList.toggle('showCart');
+})
+closeButton.addEventListener('click', () => {
+    body.classList.toggle('showCart');
 })
