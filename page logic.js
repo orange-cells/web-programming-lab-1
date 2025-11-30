0d8196d2d70bdb4c1e3a38fb5cc69ba804ce0f84
orange-cells <sokolovaaveraa@gmail.com>
2025-11-30 23:44:34 +0300

    начала реализовывать логику корзины
    у товаров появились id, кнопка добавления в корзину работает, список товаров в корзине обновляется

--- page logic.js
+++ page logic.js
@@ -1,6 +1,8 @@
 ﻿let shoppingCartImg = document.querySelector('.cart-image');
 let closeButton = document.querySelector('.shoppingCart .close')
 let body = document.querySelector('body');
+let catalog = document.querySelector('.catalog');
+let cart = [];
 
 shoppingCartImg.addEventListener('click', () => {
     body.classList.toggle('showCart');
@@ -8,3 +10,26 @@ shoppingCartImg.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     body.classList.toggle('showCart');
 })
+
+catalog.addEventListener('click', (event) => {
+    let positionClick = event.target.closest('.item');
+    let itemId = positionClick.id;
+    addToCart(itemId)
+})
+
+const addToCart = (itemId) => {
+    let putItemInCart = cart.findIndex((value) => value.itemId == itemId);
+    if (cart.length <= 0) {
+        cart = [{
+            itemId: itemId,
+            quantity: 1
+        }]
+    } else if (putItemInCart < 0) {
+        cart.push({
+            itemId: itemId,
+            quantity: 1
+        });
+    } else {
+        cart[putItemInCart].quantity++;
+    }
+}
